5503be53ccea3659f7903e299588a319b62913cc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-29 21:20:46 +0300

    Initial commit

---
+++ src/test/java/com/github/rakhmedovrs/spring/jms/SpringJmsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.rakhmedovrs.spring.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
