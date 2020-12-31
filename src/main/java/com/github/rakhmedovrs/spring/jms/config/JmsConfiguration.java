db98f4e2f2f8d54f55afb29a871b02b3ee12d915
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-31 15:20:51 +0300

    Added JmsConfiguration

---
+++ src/main/java/com/github/rakhmedovrs/spring/jms/config/JmsConfiguration.java
@@ -0,0 +1,25 @@
+package com.github.rakhmedovrs.spring.jms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/31/2020
+ */
+@Configuration
+public class JmsConfiguration
+{
+	@Bean
+	public MessageConverter messageConverter()
+	{
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+
+		return converter;
+	}
+}
